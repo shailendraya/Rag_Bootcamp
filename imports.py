dc06e1c830a4feac27a44c52fc503efe7afa9e3f
Shailendra Nagina <125447802+shailendraya@users.noreply.github.com>
2026-08-07 07:48:56 +0530

    multiagent

--- imports.py
+++ imports.py
@@ -158,3 +158,4 @@ from langchain_community.document_loaders.youtube import YoutubeLoader
 
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_core.tools import Tool
